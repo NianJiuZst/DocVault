64e6e9e5f293951c3e1f4217e401adb2c942f095
Zestia-l <2629774016@qq.com>
2025-10-17 15:15:28 +0800

    feat: 🎸 enableCORS finished

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], 
+    exposedHeaders: ['Set-Cookie'], 
+  });
   await app.listen(port);
   console.log(`服务运行在 http://localhost:${port}`);
 }
